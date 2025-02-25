9adb96bf853407cf58381b25198fb36db5e0b319
friedhar <ronfrie@proton.me>
2025-02-25 15:34:23 +0200

    add: StackBook, StackOrder

--- src/stackbook.rs
+++ src/stackbook.rs
@@ -1,1 +1,21 @@
+use std::mem::MaybeUninit;
 
+pub struct StackBook<const N: usize> {
+    bids: [MaybeUninit<StackOrder>; N],
+    asks: [MaybeUninit<StackOrder>; N],
+}
+
+pub struct StackOrder {
+    pub oid: u64,
+    pub px: u64,
+    pub sz: u64,
+}
+
+impl<const N: usize> StackBook<N> {
+    pub fn new() -> StackBook<N> {
+        StackBook {
+            bids: [const { MaybeUninit::uninit() }; N],
+            asks: [const { MaybeUninit::uninit() }; N],
+        }
+    }
+}
