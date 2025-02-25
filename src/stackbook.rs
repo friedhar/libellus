05bc271cf2da01e8b22fb6cf23fa01c5b78c5cb0
friedhar <ronfrie@proton.me>
2025-02-25 17:16:03 +0200

    add: OrdersInnerTree

--- src/stackbook.rs
+++ src/stackbook.rs
@@ -5,10 +5,25 @@ pub struct StackBook<const N: usize> {
     asks: [MaybeUninit<StackOrder>; N],
 }
 
+pub struct OrdersTreeInner<const N: usize> {
+    inner: [MaybeUninit<StackOrder>; N],
+    insert_index: usize,
+}
+
+impl<const N: usize> OrdersTreeInner<N> {
+    pub fn new() -> OrdersTreeInner<N> {
+        OrdersTreeInner {
+            inner: [const { MaybeUninit::uninit() }; N],
+            insert_index: 0,
+        }
+    }
+}
+
 pub struct StackOrder {
     pub oid: u64,
     pub px: u64,
     pub sz: u64,
+    pub next: Option<usize>,
 }
 
 impl<const N: usize> StackBook<N> {
@@ -16,6 +31,12 @@ impl<const N: usize> StackBook<N> {
         StackBook {
             bids: [const { MaybeUninit::uninit() }; N],
             asks: [const { MaybeUninit::uninit() }; N],
+        }
+    }
+
+    pub fn place(&mut self, order: StackOrder, bid: bool) {
+        for (ix, i) in self.bids.iter().enumerate() {
+            if order.px < i.px {}
         }
     }
 }
